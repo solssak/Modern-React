fa330c4c051a5e571c71ce38f59ab9659c46e4bc
hyunbook <hyunbook@hyunbooki.local>
2023-03-06 03:19:39 +0900

    여러개의 input 상태 관리하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,21 +1,10 @@
-import React from "react";
-import Hello from "./Hello";
+import React from 'react';
+import Counter from './Counter';
+import InputSample from './InputSample'
 
 function App() {
-  const name = "react";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 24, // 기본 단위 px
-    padding: "1rem", // 다른 단위 사용 시 문자열로 설정
-  };
-
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <InputSample />
   );
 }
 
